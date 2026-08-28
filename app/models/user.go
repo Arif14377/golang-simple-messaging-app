aaae5c2775da755b0c4203565239836d9beea1a0
Arif14377 <arifrahman14377@gmail.com>
2026-08-28 21:37:22 +0700

    feat: refactor user model and add session, login request/response structures

--- app/models/user.go
+++ app/models/user.go
@@ -1,18 +1,53 @@
 package models
 
 import (
+	"time"
+
 	"github.com/go-playground/validator/v10"
-	"gorm.io/gorm"
 )
 
 var validate = validator.New()
 
 type User struct {
-	gorm.Model
-	Email    string `json:"username" gorm:"unique;" validate:"required,email,min=6,max=32"`
-	Password string `json:"-" gorm:"type:text;" validate:"required,min=6"`
+	ID        uint      `json:"id" gorm:"primarykey"`
+	Username  string    `json:"username" gorm:"unique;type:varchar(20)" validate:"required,min=6,max=32"`
+	FullName  string    `json:"fullName" gorm:"type:varchar(100);" validate:"required,min=6"`
+	Password  string    `json:"password,omitempty" gorm:"type:varchar(255);" validate:"required,min=6"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
 }
 
 func (l User) Validate() error {
 	return validate.Struct(l)
+}
+
+type UserSession struct {
+	ID                  uint `gorm:"primarykey"`
+	CreatedAt           time.Time
+	UpdatedAt           time.Time
+	UserID              uint      `json:"user_id" gorm:"type:int" validate:"required"`
+	Token               string    `json:"token" gorm:"type:varchar(255)" validate:"required"`
+	RefreshToken        string    `json:"refresh_token" gorm:"type:varchar(255)" validate:"required"`
+	TokenExpired        time.Time `json:"-" validate:"required"`
+	RefreshTokenExpired time.Time `json:"-" validate:"required"`
+}
+
+func (l UserSession) Validate() error {
+	return validate.Struct(l)
+}
+
+type LoginRequest struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+func (l LoginRequest) Validate() error {
+	return validate.Struct(l)
+}
+
+type LoginResponse struct {
+	Username     string `json:"username" `
+	FullName     string `json:"full_name" `
+	Token        string `json:"token" `
+	RefreshToken string `json:"refresh_token" `
 }
