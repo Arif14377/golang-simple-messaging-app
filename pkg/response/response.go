503a5140c1f45f7a55ffd0baa80dddf9b6a73a20
Arif Rahman <arifrahman14377@gmail.com>
2026-09-07 00:08:09 +0700

    feat: add success and failure response structures with corresponding handlers

---
+++ pkg/response/response.go
@@ -0,0 +1,29 @@
+package response
+
+import "github.com/gofiber/fiber/v3"
+
+type SuccessResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+}
+
+type FailureResponse struct {
+	Success bool
+	Message string
+}
+
+func SendSuccessResponse(ctx fiber.Ctx, statusCode int, message string, data any) error {
+	return ctx.Status(statusCode).JSON(SuccessResponse{
+		Success: true,
+		Message: message,
+		Data:    data,
+	})
+}
+
+func SendFailureResponse(ctx fiber.Ctx, statusCode int, message string) error {
+	return ctx.Status(statusCode).JSON(FailureResponse{
+		Success: false,
+		Message: message,
+	})
+}
